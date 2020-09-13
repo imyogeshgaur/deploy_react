3b0f1bcc5a9489ad65e30fb9c2021030eeb233de
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-09-13 11:19:20 +0530

    Updates

---
+++ precache-manifest.aa9389d725b3f0b3a5d8af11460f2801.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d762ffbd2fd1635ac0e6c4459ef52875",
+    "url": "/deploy_react/index.html"
+  },
+  {
+    "revision": "1cfa33caaf487f3d936b",
+    "url": "/deploy_react/static/css/main.18e99303.chunk.css"
+  },
+  {
+    "revision": "e9cd1e54d3e809ca1aad",
+    "url": "/deploy_react/static/js/2.c8bc91a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy_react/static/js/2.c8bc91a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfa33caaf487f3d936b",
+    "url": "/deploy_react/static/js/main.ee6e0565.chunk.js"
+  },
+  {
+    "revision": "99e880bb20407b57bc5e",
+    "url": "/deploy_react/static/js/runtime-main.fdb7c28b.js"
+  },
+  {
+    "revision": "3941a20e520b2189591ab54fb3b46f7d",
+    "url": "/deploy_react/static/media/pic4.3941a20e.jpg"
+  },
+  {
+    "revision": "e83c8d179a7174c90582a30bdee9eefa",
+    "url": "/deploy_react/static/media/pic5.e83c8d17.jpg"
+  },
+  {
+    "revision": "807b3a3e4ca00215e3cb4c604cb17ffb",
+    "url": "/deploy_react/static/media/pic6.807b3a3e.jpg"
+  },
+  {
+    "revision": "497290937f37df797fef2ee817b71d32",
+    "url": "/deploy_react/static/media/pic7.49729093.jpg"
+  }
+]);
